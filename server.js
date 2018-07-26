f9c35282dd992ce4413b2aaf0cf675f938ebfca4
Vadim Liakhovich <v.liakhovich@yandex.ru>
2018-07-26 16:55:22 +0300

    fix app refresh on server

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(`${__dirname}/dist/movie-app-project`));
+
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/movie-app-project/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on http://localhost:${PORT}`);
